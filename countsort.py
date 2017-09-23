883472aae5410532aa12e4a800a8b5ee9554c603
gwangaj <gwangaj@connect.ust.hk>
2017-09-23 15:41:48 +0800

    countsort

---
+++ countsort.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# In[120]:
+
+def count_sort(array):
+    maximum = max(array)
+    minimum = min(array)
+    count_array = [0]*(maximum-minimum+1)
+
+    for val in array:
+        count_array[val-minimum] += 1
+
+    sorted_array = []
+    for i in range(minimum, maximum+1):
+        if count_array[i-minimum] > 0:
+            for j in range(0, count_array[i-minimum]):
+                sorted_array.append(i)
+    return sorted_array
+
+
+# In[ ]:
+
+
+
