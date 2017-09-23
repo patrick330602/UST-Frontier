8f577f43dd2169a35f2a67dd777cc2ab026d9b42
Patrick J. Wu <wotingwu@live.com>
2017-09-23 10:11:34 +0800

    Modification

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
 import requests
 import json
 import os
+
+import quicksort
 
 
 app  = Flask(__name__)
@@ -10,3 +12,8 @@ app  = Flask(__name__)
 @app.route('/')
 def index():
 	return "Thank you for visiting this site for demo!"
+
+@app.route('/sort', methods=['POST'])
+def sort():
+	content = quicksort.quickSort([1,2,3,4])
+	return content
---
+++ team_register.py
@@ -0,0 +1,10 @@
+import requests
+url = 'http://cis2017-coordinator.herokuapp.com/api/teams'
+data = '''
+{
+    "name": "UST Frontier",
+    "url": "http://ust-frontier.herokuapp.com",
+    "members": ["Liu Chengzhong","Wang GUanzhi", "Wu Jinming"]
+}'''
+response = requests.post(url, data=data)
+print 
