c4d90ff5510dde3281cfee0bbe3500a5b59f0ba7
Patrick J. Wu <wotingwu@live.com>
2017-09-24 00:22:40 +0800

    modification

--- knapsack.py
+++ knapsack.py
@@ -7,17 +7,15 @@ def KnapsackFrac(v, w, W):
     for i in range(n):
         order.append(float(v[i] * 10000/w[i]))
     order, v, w =zip(*sorted(zip(order, v, w)))
-    for i in range(n):
-        wi = w[i]
+    for r in range(n):
+        wi = w[r]
         if wi >= W:
-            a = int(v[i] * W)
-            value += a
+            value = value + int(order[r] /10000 * W)
             return value
         else:
-            b = int(v[i] * wi)
-            value += b
-            W -= wi
-    return int(value)
+            value = value + int( order[r] /10000* wi)
+            W = W - wi
+    return value
 
 a = 4
 b = [1,3,5,2]
