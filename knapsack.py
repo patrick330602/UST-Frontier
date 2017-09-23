529a74a435abeefddba9a96adcb6deec1d4f82be
Patrick J. Wu <wotingwu@live.com>
2017-09-23 22:41:14 +0800

    modification

--- knapsack.py
+++ knapsack.py
@@ -2,19 +2,15 @@ def KnapsackFrac(v, w, W):
     dictionary = {} #value: weight
     total_value = 0
     for i in range(len(v)):
-        ratio = int(v[i]) / int(w[i])
-        dictionary[ratio] = w[i]
-    sorted_key = sorted(list(dictionary.keys()), reverse=True)
-    for j in sorted_key:
-        wi = dictionary[j]
-        if wi >= W:
-            total_value = total_value + int(j * W)
-            return total_value
-        else:
+        rt = int(v[i]) / int(w[i])
+        dt[rt] = w[i]
+    sk = sorted(list(dt.keys()), reverse=True)
+    for j in sk:
+        wi = dt[j]
+        if wi < W:
             total_value = total_value + int(j * wi)
             W -= wi
+        else:
+            total_value = total_value + int(j * W)
+            return total_value
     return total_value
-weight_max = 4
-values = [1,3,5,2]
-weight = [200, 240, 150, 140]
-print(KnapsackFrac(values, weight, weight_max))
