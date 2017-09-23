687a6b7d92c8b8fd81041c0e3cb53ae372a39c13
Patrick J. Wu <wotingwu@live.com>
2017-09-24 01:15:19 +0800

    modification

--- knapsack.py
+++ knapsack.py
@@ -1,26 +1,18 @@
+import numpy as np
+
 def KnapsackFrac(v, w, W):
-    weight = 0.0
-    value = 0.0
-    knapsack = []
-    index = 0
-    order = []
-    n = len(v)
-    for i in range(n):
-        order.append(float(v[i] * 10000/w[i]))
-    order, v_temp, w_temp =zip(*sorted(zip(order, v, w)))
-    
-    while (weight < W) and (index < n):
-        if weight + w[order[index]] <= W:
-            knapsack.append((order[index], 1.0))
-            weight = weight + w[order[index]]
-            value = value + v[order[index]]
-        else:
-            fraction = (W - weight) / w[order[index]]
-            knapsack.append((order[index], fraction))
-            weight = W
-            value = value + v[order[index]] * fraction
-        index = index + 1
-    return value
+    p = np.empty(len(v))
+    for i in range(len(v)):
+        p[i] = -v[i]/w[i]
+    idx = np.argsort(p)
+    val = 0
+    for i in idx:
+        use_w = min(W, w[i])
+        W -= use_w
+        val += v[i]/w[i]*use_w
+        if W<=0: break
+    return val
+
 
 #a = 5
 #b = [1,3,5,2]
