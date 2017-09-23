71e2914d16da0c1d855cb61194297462a0c706b6
Patrick J. Wu <wotingwu@live.com>
2017-09-23 22:12:22 +0800

    modification

--- knapsack.py
+++ knapsack.py
@@ -1,18 +1,25 @@
-import operator
+import decimal
 def KnapsackFrac(v, w, W):
-    value = 0
+    value = 0.0000000000
     order=[]
-    order = [v/w for v, w in zip(v,w)]
+    n = len(v)
+    for j in range(n):
+        order.append(decimal.Decimal(v[j])/decimal.Decimal(w[j]))
     dict_order = zip(order, w)
     sorted(dict_order, key=lambda x: x[0])
-        for i in dict_order:
-            wi = i[1]
-            if wi >= W:
-                a = int(i[0] * W)
-                value += a
-                return value
-            else:
-                b = int(i[0] * wi)
-                value += b
-                W -= wi
+    for i in dict_order:
+        wi = i[1]
+        if wi >= W:
+            a = int(i[0] * W)
+            value += a
+            return value
+        else:
+            b = int(i[0] * wi)
+            value += b
+            W -= wi
     return value
+
+weight_max = 4
+values = [1,3,5,2]
+weight = [200, 240, 150, 140]
+print(KnapsackFrac(values, weight, weight_max))
