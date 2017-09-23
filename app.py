c24e4cef3283db00d44c1792a0a7098c34bc6b7b
Patrick J. Wu <wotingwu@live.com>
2017-09-23 11:21:05 +0800

    test

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ import quicksort
 app  = Flask(__name__)
 
 
-@app.route('/')
-def index():
-	return "Thank you for visiting this site for demo!"
-
 @app.route('/sort', methods=['POST'])
 def sort():
-	content = request.get_json()
-	
-	return content
+	if request.method == 'POST':
+		content = request.get_json()
+		data  = json.loads(content)
+		new = quicksort.quickSort(data)
+		return jsonify(new)
+	else:
+		pass
